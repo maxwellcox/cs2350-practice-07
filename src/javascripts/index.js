ec7d81bb513cc7b3e8cbfc46e64da5e795bc5853
maxwellcox <cox2mdc@gmail.com>
2020-03-27 23:52:15 -0600

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,7 +1,48 @@
 // Required by Webpack - do not touch
-require.context('../', true, /\.(html|json|txt|dat)$/i)
-require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
-require.context('../stylesheets/', true, /\.(css|scss)$/i)
+// require.context('../', true, /\.(html|json|txt|dat)$/i)
+// require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
+// require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+//import "bootstrap"
+
+const displayRandomBox = function(){
+    let r = Math.random()
+    if(r < 0.5)
+        document.write('<div class="yellow box"></div>')
+    else
+        document.write('<div class="blue box"></div>')
+}
+
+function displayNBoxesUsingFor(n){
+    for(let i = 0; i < n; i++){
+        displayRandomBox()
+    }
+}
+
+function displayNBoxesUsingWhile(n){
+    let j = 0
+    while(j < n){
+        displayRandomBox()
+        j++
+    }
+}
+
+function displayNBoxesUsingDoWhile(n){
+    let k = 0
+    do{
+        displayRandomBox()
+        k++
+    }while(k < n)
+}
+
+displayNBoxesUsingFor(15)
+displayNBoxesUsingWhile(15)
+displayNBoxesUsingDoWhile(15)
+
+const square = function(x){
+    return x*x
+}
+
+alert(square(10))
